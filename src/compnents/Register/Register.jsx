fbdb6beef18566752d5a31cf58af8772151e82c5
Moaaz-Ismail <maaz1421@gmai.com>
2026-03-15 00:23:49 +0200

    create register schema

--- src/compnents/Register/Register.jsx
+++ src/compnents/Register/Register.jsx
@@ -1,5 +1,25 @@
 import React from "react";
+import styles from "./Register.module.css"
+import {useForm} from 'react-hook-form'
+import z from 'zod'
+
+let schema = z.object({
+  name:z.string().min(3,"Name Minimum 3 Charecters").max(30,"Name Max 30 Charecters"),
+  email:z.string().email("please Enter Valid Email"),
+  password:z.regex()
+})
+
 
 export default function Register() {
+
+  let registerForm = useForm({
+    defualtVslues:{
+      "name": " ",
+      "email": "",
+      "password": ""
+    },
+    resolver:zodResolver(schema)
+  })
+
   return <div>Register</div>;
 }
