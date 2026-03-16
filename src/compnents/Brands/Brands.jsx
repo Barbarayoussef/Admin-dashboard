83aa17d81db14f0fbe19f4a2450a9f580e46925f
Barbarayoussef <barayoussef12@yahoo.com>
2026-03-16 23:28:20 +0200

    add brand and edit in subcategory

--- src/compnents/Brands/Brands.jsx
+++ src/compnents/Brands/Brands.jsx
@@ -1,5 +1,16 @@
 import React from "react";
-
+import { z } from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+let schema = z.object({
+  name: z.string().min(4, "Name must be at least 4 characters"),
+});
 export default function Brands() {
-  return <div>Brands</div>;
+  let { register, formState, handleSubmit, reset } = useForm({
+    defaultValues: {
+      name: "",
+    },
+    resolver: zodResolver(schema),
+  });
+  return <></>;
 }
